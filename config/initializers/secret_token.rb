3852f62e8a8699688ed2999ca4c688433be49490
waho.me <me@waho.me>
2014-02-03 13:04:52 +0000

    Initialise repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testapp::Application.config.secret_key_base = 'e7372a815d6055124008e87ab8155009f9233d3813c99afcf7a7a97d2f5e5bda49c988fde5ab96cbf048253109895ed40bfee01ae9556498e69038387779cc12'
